362798ffe2dc0ee893758dc949a33ce246a1ff91
Álvaro de la Cruz Herranz <alvarogfx2@gmail.com>
2024-10-26 20:48:38 +0200

    secciones
    ha añadido la seccion de Featured Works, he cambiado el color al typed js y he cambiado por completo la pagina de fotografía

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
 var typed = new Typed('#element', {
     strings: ['<i>Your vision, our passion.Partnering for design excellence.</i>'],
-    typeSpeed: 65,
+    typeSpeed: 50,
     loop: true
 });
+
   
