b310ec7228e45d5f151af0c8db8a05c7fd7afa97
Kei-ssen <lk95tae@gmail.com>
2024-10-27 23:40:35 +0100

    Modal(not finished), top btn, contact section, intro section

--- js/script.js
+++ js/script.js
@@ -6,7 +6,56 @@ var typed = new Typed('#element', {
 });
 
 
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// CONTACT BTN
+const btnContact = document.querySelector(".btn-modal");
+        //console.log(btnModal);
+        btnModal.addEventListener("click", openModal);
+
+        //declaramos funcion open modal
+        function openModal() {
+            const modalWindow = document.querySelector("#modalWindow");
+            modalWindow.classList.add("show-modal");
+
+        }
+
+        const btn_closeModal = document.querySelector("#modalWindow .close");
+        btn_closeModal.addEventListener("click", closeModal);
+
+        function closeModal() {
+            const modalWindow = document.querySelector("#modalWindow");
+            modalWindow.classList.remove("show-modal");
+        }
+
+    // El objeto window hace referenci a toda la ventana de la web
+        window.addEventListener("click", function(event){
+            console.log("u clicked");
+            const modalWindow = document.querySelector("#modalWindow");
+            const condition = event.target === modalWindow;
+
+            if (condition) {
+                closeModal()
+            }
+
+        });
 
   
